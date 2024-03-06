c3e9fa139aeecea7e574dd61f6bc227817bf260b
giovanaFurlan <giovananelofurlan@gmail.com>
2024-03-06 12:34:20 -0300

    fix: Teste

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -20,13 +20,6 @@ const theme = extendTheme({
   fonts: {
     heading: `'Lato', sans-serif`,
     body: `'Lato', sans-serif`,
-  },
-  breakpoints: {
-    sm: "320px",
-    md: "768px",
-    lg: "960px",
-    xl: "1200px",
-    "2xl": "1800px"
   }
 });
 
